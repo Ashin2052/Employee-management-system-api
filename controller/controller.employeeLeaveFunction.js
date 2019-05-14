9801cbf48e2dd9eb69d79c01dd234083a222beec
sujan bajracharya <sujanbajracharya1921@gmail.com>
2019-05-14 16:00:13 +0545

    test

--- controller/controller.employeeLeaveFunction.js
+++ controller/controller.employeeLeaveFunction.js
@@ -1,62 +1,62 @@
-const employeeLeaveModels=require('../model/employeeLeave');
-const employeeContact=require('../model/employeeDetails');
+const employeeLeaveModels = require("../model/employeeLeave");
+const employeeContact = require("../model/employeeDetails");
 
-const jwt=require('jsonwebtoken');
-const sec=require('../config')
+const jwt = require("jsonwebtoken");
+const sec = require("../config");
 
-class employeeLeave
-{
-    constructor(){}
+class employeeLeave {
+  constructor() {}
 
-    ApplyLeave(payload , userId,fullName)
-    {
-        return new Promise((resolve,reject)=>
-        {  
-            
-            employeeLeaveModels({
-                eid:userId,
-                ReturnDate:new Date(payload.ReturnDate),
-                LeaveDate:new Date(payload.LeaveDate),
-                Description:payload.Description
-            }).save()
-            .then(d=>resolve(d))
-            .catch(e=>reject(e))
-            console.log("break")
+  ApplyLeave(payload, userId, fullName) {
+    console.log(payload, userId, "user leave data");
+    return new Promise((resolve, reject) => {
+      employeeLeaveModels({
+        eid: userId,
+        ReturnDate: new Date(payload.start_date),
+        LeaveDate: new Date(payload.end_date),
+        Description: payload.reason
+      })
+        .save()
+        .then(d => resolve(d))
+        .catch(e => {
+          console.log(e, "apply leave error");
+          reject(e);
+        });
+      console.log("break");
+    });
+  }
+
+  findEmployeeAllLeave() {
+    return new Promise((resolve, reject) => {
+      console.log("nkj");
+      employeeLeaveModels
+        .find()
+        .populate("eid")
+        .then(d => {
+          resolve(d);
+          console.log(d);
         })
-    }
-
-    findEmployeeAllLeave(){
-        return new Promise((resolve,reject)=>
-{
-        console.log("nkj")
-        employeeLeaveModels.find().populate('eid' )
-        .then(d=>{resolve(d)
-            console.log(d)
-    })
-      .catch(e=>reject(e))
-       
-})
-}
-MakeAdmin(payload)
-{
-    return new Promise((resolve,reject)=>
-    {
-        console.log("ada")
-        employeeContact.findOneAndUpdate(
-            {
+        .catch(e => reject(e));
+    });
+  }
+  MakeAdmin(payload) {
+    return new Promise((resolve, reject) => {
+      console.log("ada");
+      employeeContact
+        .findOneAndUpdate(
+          {
             _id: payload.userId
-        }
-        ,
-        {
-            $set:payload
-        },
-        {
-            new:true
-        })
-        .then(d=>resolve(d))
-        .catch(e=>reject(e));
-    })
+          },
+          {
+            $set: payload
+          },
+          {
+            new: true
+          }
+        )
+        .then(d => resolve(d))
+        .catch(e => reject(e));
+    });
+  }
 }
-
-}
-module.exports=new employeeLeave();
+module.exports = new employeeLeave();

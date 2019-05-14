826b5f38fbbcc4ca4bee86e5753b66d8d49a15c6
Ashin Mahat <mahat.ashin@gmail.com>
2019-05-14 11:27:28 +0545

    leave employee

--- controller/controller.employeeLeave.js
+++ controller/controller.employeeLeave.js
@@ -24,11 +24,19 @@ router.get('/AppliedEmployeeList',(req,res,next)=>
 router.put('/Makeadmin',validateUser,(req,res,next)=>
 { 
     console.log(req.isadmin,'isadmin')
+    if(req.isadmin=="true"){
     employeeLeavefunction.MakeAdmin(req.body)
     .then(d=>res.json(d))
     .catch(next);
+    }
+    else{
+      res.json("you are not admin");
+    }
 })
-router.update
 
+// router.put('/Approve/:Id',(req,res,next)=>
+// {
 
+// })
+ 
 module.exports=router;
